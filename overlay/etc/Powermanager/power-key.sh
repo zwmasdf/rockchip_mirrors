0623dacfe6cabeb0f3b735b06626b0b5a343ec18
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-01-05 03:27:35 +0800

    power-key.sh: Fix key event races
    On some platforms(e.g. rk356x), the wakeup key event is much slower
    than it should be, which would bypass the pm-utils's lock flow.
    
    Also there's a race in press key event handler might cause unexpected
    shutdown.
    
    Change-Id: I05f70d88a680db3b01ef2fa2662651520f648486
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- overlay/etc/Powermanager/power-key.sh
+++ overlay/etc/Powermanager/power-key.sh
@@ -2,31 +2,26 @@
 
 EVENT=${1:-short-press}
 
-logger -t $(basename $0) "[$$]: Received power key event: $@..."
-
 TIMEOUT=3 # s
 PIDFILE="/tmp/$(basename $0).pid"
+LOCKFILE=/tmp/.power_key
 
 short_press()
 {
 	logger -t $(basename $0) "[$$]: Power key short press..."
 
 	if type pm-suspend &>/dev/null; then
-		LOCK=/var/run/pm-utils/locks/pm-suspend.lock
 		SUSPEND_CMD="pm-suspend"
 	else
-		LOCK=/tmp/.power_key
-		PRE_SUSPEND="touch $LOCK"
 		SUSPEND_CMD="echo -n mem > /sys/power/state"
-		POST_SUSPEND="{ sleep 2 && rm $LOCK; }&"
 	fi
 
-	if [ ! -f $LOCK ]; then
+	if [ ! -f $LOCKFILE ]; then
 		logger -t $(basename $0) "[$$]: Prepare to suspend..."
 
-		sh -c "$PRE_SUSPEND"
+		touch $LOCKFILE
 		sh -c "$SUSPEND_CMD"
-		sh -c "$POST_SUSPEND"
+		{ sleep 2 && rm $LOCKFILE; }&
 	fi
 }
 
@@ -39,15 +34,24 @@ long_press()
 	poweroff
 }
 
+logger -t $(basename $0) "[$$]: Received power key event: $@..."
+
 case "$EVENT" in
 	press)
-		start-stop-daemon -K -q -p $PIDFILE
+		# Lock it
+		exec 3<$0
+		flock -x 3
+
+		start-stop-daemon -K -q -p $PIDFILE || true
 		start-stop-daemon -S -q -b -m -p $PIDFILE -x /bin/sh -- \
 			-c "sleep $TIMEOUT; $0 long-press"
+
+		# Unlock
+		flock -u 3
 		;;
 	release)
 		# Avoid race with press event
-		sleep .2
+		sleep .5
 
 		start-stop-daemon -K -q -p $PIDFILE && short_press
 		;;
