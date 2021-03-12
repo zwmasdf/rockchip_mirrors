16d7cd621bb8889078ec1328f9c1c1b9ab19c18c
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-03-13 06:58:44 +0800

    Revert "packages: Add gl4es"
    This reverts commit 44398d80f60e2658936ec76b57e5b1d177a0991c.
    
    Too sad, it's still not stable enough for us.
    
    Change-Id: I5c77d1c68fb45c3e468570e91c6c0ec2b53b56c3
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- mk-rootfs-buster.sh
+++ mk-rootfs-buster.sh
@@ -124,9 +124,6 @@ dpkg -i  /packages/gst-base/*.deb
 #apt-mark hold gstreamer1.0-x
 apt-get install -f -y
 
-# gl4es
-cp -rf /packages/gl4es/* /
-
 #---------Camera---------
 echo -e "\033[36m Install camera.................... \033[0m"
 apt-get install cheese v4l-utils -y
